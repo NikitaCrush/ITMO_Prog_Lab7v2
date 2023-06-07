86b8d6592de3c206a08bc966952822a807d2a552
artemijr <arrtemijr@gmail.com>
2023-06-07 22:27:42 +0300

    added Manager for database

---
+++ Server/src/main/kotlin/databaseManager/DatabaseManager.kt
@@ -0,0 +1,27 @@
+package databaseManager
+
+import java.io.File
+import java.sql.Connection
+import java.sql.DriverManager
+
+class DatabaseManager {
+    var connection: Connection? = null
+        private set
+    init {
+        val lines = File("db_credentials.txt").readLines()
+        val url = lines[0]
+        val user = lines[1]
+        val password = lines[2]
+
+//        try {
+            connection = DriverManager.getConnection(url, user, password)
+            println("Connected to the PostgresSQL server successfully.")
+//        } catch (e: SQLException) {
+//            println(e.message)
+//        }
+    }
+
+    fun close() {
+        connection?.close()
+    }
+}
